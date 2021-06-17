2453cfcc5d13538ba2063adcd8a9c59abd4b4f8b
Gnabriel <elleb@kth.se>
2021-06-17 14:53:22 +0200

    Added input validations

--- src/Lab2.java
+++ src/Lab2.java
@@ -4,23 +4,44 @@ public class Lab2 {
     public static void main(String[] args) {
         // Scanner to read input.
         Scanner scanner = new Scanner(System.in);
+
         // Get 1st input from scanner.
+        String sentence;
         while (true) {
+            // Print 1st instructions.
             System.out.println("Skriv in en mening:");
             try {
-                String sentence = scanner.nextLine();
+                sentence = scanner.nextLine();
+                if (sentence.length() == 0) {
+                    throw new java.lang.RuntimeException("Felaktig input.");
+                }
                 break;
             }
-            catch (java.util.InputMismatchException e) {
+            catch (java.lang.RuntimeException e) {
                 System.out.println("Felaktig input. Ange en mening.");
-                scanner.nextLine();
             }
         }
 
-        // Print 2nd instructions.
-        System.out.println("Skriv in ett tecken:");
         // Get 2nd input from scanner.
-        char character = scanner.next(".").charAt(0);
+        String input;
+        char character;
+        while (true) {
+            // Print 2nd instructions.
+            System.out.println("Skriv in ett tecken:");
+            try {
+                input = scanner.nextLine();
+                if (input.length() == 1) {
+                    character = input.charAt(0);
+                }
+                else {
+                    throw new java.lang.RuntimeException("Felaktig input.");
+                }
+                break;
+            }
+            catch (java.lang.RuntimeException e) {
+                System.out.println("Felaktig input. Ange ett tecken.");
+            }
+        }
 
         // Count character occurrences.
         int charCount = 0;
