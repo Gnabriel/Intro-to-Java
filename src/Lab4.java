5c808bc2b9efb41748a413427f521b9fedde411f
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-07-17 12:18:34 +0200

    Changed JavaDoc comment format

--- src/Lab4.java
+++ src/Lab4.java
@@ -6,13 +6,26 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * Text-based glossary excercise program. The program will show a Swedish
+ * word and let the user input the English translation of that word.
+ * Then it will show if the user's translation was wrong, correct or
+ * partially correct. A word is seen as partially correct if it is one
+ * Levenshtein distance from the correct word. Once finished it will
+ * display statistics of the user's score.
+ * 
+ * The glossary excercise if finished after 10 words or by pressing 'Q'.
+ * 
+ * Assignment 4 in the course Inledande programmering med Java VT2021.
+ *
+ * @author Gabriel Ellebrink
+ */
 public class Lab4 {
-    /**
-     * Assignment 4 in the course Inledande programmering med Java VT2021.
-     *
-     * @author Gabriel Ellebrink
-     */
 
+    /**
+     * Class to create glossaries which can store words
+     * along with its translation(s).
+     */
     public static class Glossary {
         Map<String, List<String>> glossaryDict;
         List<String> glossaryWords;
@@ -20,34 +33,30 @@ public class Lab4 {
         int wordCount;
 
         public Glossary() {
-            /**
-             * Class to create glossaries which can store words
-             * along with its translation(s).
-             */
             this.glossaryDict = new HashMap<>();
             this.glossaryWords = new ArrayList<>();
             this.currentWord = 0;
             this.wordCount = 0;
         }
 
+        /**
+         * Adds a word with its translation(s) to the glossary.
+         * 
+         * @param word              word to be translated
+         * @param translations      translation(s) of the word
+         */
         public void addWord(String word, List<String> translations) {
-            /**
-             * Adds a word with its translation(s) to the glossary.
-             * 
-             * @param word              word to be translated
-             * @param translations      translation(s) of the word
-             */
             this.glossaryDict.put(word, translations);
             this.glossaryWords.add(word);
             this.wordCount++;
         }
 
+        /**
+         * Gets the next word in the glossary.
+         * 
+         * @return      next String in glossary
+         */
         public String getNextWord() {
-            /**
-             * Gets the next word in the glossary.
-             * 
-             * @return      next String in glossary
-             */
             String nextWord = "";
             if (this.currentWord <= this.wordCount) {
                 nextWord = this.glossaryWords.get(this.currentWord);
@@ -56,27 +65,27 @@ public class Lab4 {
             return nextWord;
         }
 
+        /**
+         * Gets the correct translation(s) of a word in the glossary.
+         * 
+         * @return      correct translation(s) of a word
+         */
         public List<String> getAnswer(String word) {
-            /**
-             * Gets the correct translation(s) of a word in the glossary.
-             * 
-             * @return      correct translation(s) of a word
-             */
             return this.glossaryDict.get(word);
         }
         
+        /**
+         * Checks a user submitted translation against
+         * the correct translations in the glossary.
+         * 
+         * @param word      word to be translated
+         * @param guess     user submitted translation of the word
+         * @return          1.0f if the guess is correct;
+         *                  0.5f if the guess is almost correct,
+         *                  i.e within 1 Levenshtein Distance;
+         *                  0.0f otherwise
+         */
         public float checkGuess(String word, String guess) {
-            /**
-             * Checks a user submitted translation against
-             * the correct translations in the glossary.
-             * 
-             * @param word      word to be translated
-             * @param guess     user submitted translation of the word
-             * @return          1.0f if the guess is correct;
-             *                  0.5f if the guess is almost correct,
-             *                  i.e within 1 Levenshtein Distance;
-             *                  0.0f otherwise
-             */
             List<String> correctTranslations = this.glossaryDict.get(word);
             for (String translation : correctTranslations) {
                 if (guess.equalsIgnoreCase(translation)) {
@@ -89,17 +98,17 @@ public class Lab4 {
             return 0.0f;
         }
 
+        /**
+         * Checks a user submitted translation against
+         * the correct translations in the glossary.
+         * 
+         * @param translation       correct translation
+         * @param guess             user submitted translation of the word
+         * @return                  true if guess is within 1 Levenshtein
+         *                          Distance of the correct translation;
+         *                          false otherwise
+         */
         private static boolean partialCheck(String translation, String guess) {
-            /**
-             * Checks a user submitted translation against
-             * the correct translations in the glossary.
-             * 
-             * @param translation       correct translation
-             * @param guess             user submitted translation of the word
-             * @return                  true if guess is within 1 Levenshtein
-             *                          Distance of the correct translation;
-             *                          false otherwise
-             */
             LevenshteinDistance stringComparator = LevenshteinDistance.getDefaultInstance();
             if (stringComparator.apply(translation, guess) == 1) {
                 return true;
@@ -108,24 +117,24 @@ public class Lab4 {
         }
     }
 
+    /**
+     * Class to create translations excercises by building
+     * glossaries and getting user inputs and validating them.
+     */
     public static class TranslationExcercise {
         Glossary glossary;
         Scanner scanner;
 
         public TranslationExcercise() {
-            /**
-             * Class to create translations excercises by building
-             * glossaries and getting user inputs and validating them.
-             */
             this.glossary = new Glossary();
             this.scanner = new Scanner(System.in);
             buildGlossary();
         }
 
+        /**
+         * Creates a glossary by adding words with its translation(s).
+         */
         public void buildGlossary() {
-            /**
-             * Creates a glossary by adding words with its translation(s).
-             */
             this.glossary.addWord("bil", Arrays.asList("car"));
             this.glossary.addWord("hus", Arrays.asList("house"));
             this.glossary.addWord("springa", Arrays.asList("run"));
@@ -138,12 +147,12 @@ public class Lab4 {
             this.glossary.addWord("snäll", Arrays.asList("kind"));
         }
 
+        /**
+         * Gets an input from the user.
+         * @param prompt        user instructions for the input
+         * @return              String provided by the user
+         */
         public String getInput(String prompt) {
-            /**
-             * Gets an input from the user.
-             * @param prompt        user instructions for the input
-             * @return              String provided by the user
-             */
             String userInput;
             while (true) {
                 // Print input instructions.
@@ -162,10 +171,10 @@ public class Lab4 {
             return userInput;
         }
 
+        /**
+         * Starts the glossary excercise for English words.
+         */
         public void englishExcercise() {
-            /**
-             * Starts the glossary excercise for English words.
-             */
             System.out.println("** GLOSÖVNING - ENGELSKA **");
             System.out.println("Skriv det engelska ordet. Avsluta programmet genom att skriva Q.");
             String userInput;
@@ -201,6 +210,9 @@ public class Lab4 {
         }
     }
 
+    /**
+     * Creates a TranslationExcercise instance and runs the glossary excercise.
+     */
     public static void main(String[] args) {
         TranslationExcercise excercise = new TranslationExcercise();
         excercise.englishExcercise();
